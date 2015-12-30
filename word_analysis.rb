0db65880909078a560c318c09db75da7a9ae5ce3
Dan Pickett <dan.pickett@launchacademy.com>
2015-12-30 18:14:53 -0500

    word analysis exercise

---
+++ word_analysis.rb
@@ -0,0 +1,8 @@
+file_path = File.join(File.dirname(__FILE__), 'tale_of_two_cities.txt')
+story_contents = File.read(file_path)
+
+# what are the 3 most frequently used words?
+# how many times do all of the vowels appear?
+# what is the most frequently used vowel?
+# what is the average vowel frequency?
+# how many occurrences of specific punctuation marks are there?
